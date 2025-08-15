f49c2df358f96b5e061c53631b9f64ceb5d24eb6
dhr <2322336498@qq.com>
2025-08-15 16:15:00 +0800

    添加电机报警信号读取

--- system.c
+++ system.c
@@ -97,10 +97,10 @@ ISR(CONTROL_INT_vect)
   // 只有当消抖计数器为0时才处理稳定输入
   // print_uint8_base2_ndigit(pin, 8);
   // printString("A\r\n");
-  // if (debounce_counter == 0 && sys.state != STATE_ALARM && pin_state) {
-  //     pin_state = false;
-  //     handle_stable_input(pin);
-  // }
+  if (debounce_counter == 0 && sys.state != STATE_ALARM && pin_state) {
+      pin_state = false;
+      handle_stable_input(pin);
+  }
 }
 
 // 处理稳定输入的函数
@@ -114,9 +114,9 @@ void handle_stable_input(uint8_t pin)
   sys.drawerStatus = pin & (1 << DRAWER_DETECT_BIT);
   if (!(sys_rt_exec_alarm))
   {
-    // uint8_t stopStatus = (pin & (1 << 4 | 1 << 5 | 1 << 6)) || (~pin & (1 << 7));
+    uint8_t stopStatus = (pin & (1 << 4 | 1 << 5 | 1 << 6)) || (~pin & (1 << 7));
     // uint8_t stopStatus = (~pin & (1 << 4 | 1 << 5 | 1 << 6 | 1 << 7));
-    uint8_t stopStatus = (~pin & 1 << 7);
+    // uint8_t stopStatus = (~pin & 1 << 7);
     // print_uint8_base2_ndigit(stopStatus, 8);
     // printString("S\r\n");
     // 检查限位引脚状态
