36a3596ab4fe20a5c31793ced13119cd915fa639
dhr <2322336498@qq.com>
2025-09-06 18:01:36 +0800

    perf: 修改A轴参数

--- defaults.h
+++ defaults.h
@@ -443,8 +443,8 @@
 #endif
 
 #ifdef DEFAULTS_ABC_AXIS
-#define DEFAULT_A_STEPS_PER_MM 177.77
-#define DEFAULT_A_MAX_RATE 4000.0                // mm/min
+#define DEFAULT_A_STEPS_PER_MM 180
+#define DEFAULT_A_MAX_RATE 2000.0                // mm/min
 #define DEFAULT_A_ACCELERATION (400.0 * 60 * 60) // 10*60*60 mm/min^2 = 10 mm/sec^2
 #define DEFAULT_A_MAX_TRAVEL 200.0              // mm
 
