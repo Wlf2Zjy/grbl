0478e60bd24fa80af88f2937a6235499dcd8514d
dhr <2322336498@qq.com>
2025-09-09 15:14:59 +0800

    fix: rfid读取添加关灯

--- rfid_control.c
+++ rfid_control.c
@@ -219,6 +219,7 @@ void read_all_rfid(){
     set_flip(0);
     protocol_buffer_synchronize();
     set_rfid(0);
+    set_tool_leds(LED_OFF, LED_OFF, LED_OFF, LED_OFF, LED_OFF); 
     for (uint8_t i = 0; i < TOOL_NUM-1; i++)
     {
         memset(return_data, 0, 8);     
