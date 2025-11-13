cd63b29960eba135929cf23a657bf4158c6e6e3d
dhr <2322336498@qq.com>
2025-11-13 17:43:11 +0800

    feat: 删除无用注释

--- tool_change.c
+++ tool_change.c
@@ -197,27 +197,6 @@ void change_tool(uint8_t tool_number)
         return;
       }
     }
-    // if(sys.isRunGcode){
-    //   // RFID运动到刀旁边
-    //   unsigned long nowTime = getTime(); // 记录开始时间
-    //   unsigned long useTime = nowTime - sys.startTime;
-    //   uint16_t minutes = useTime / 60;
-  
-    //   char y_char[20], command[80];
-    //   uint8_t return_data[8];
-    //   gc_execute_line("G90G53G0Z-5");
-    //   set_flip(0);
-    //   protocol_buffer_synchronize();
-    //   set_rfid(0);
-    //   memset(return_data, 0, 8);
-    //   // 移动刀位置
-    //   float2string(settings.tool_y[settings.tool - 1] - settings.rfid_offset, y_char, 3);
-    //   sprintf(command, "G90G53G0Y%s", y_char);
-    //   gc_execute_line(command);
-    //   protocol_buffer_synchronize();
-    //   rfid_write(settings.tool, minutes);
-    //   set_rfid(1);
-    // }
   }
   else
   {
@@ -233,27 +212,6 @@ void change_tool(uint8_t tool_number)
         return;
       }
     }
-    // if(sys.isRunGcode){
-    //   // RFID运动到刀旁边
-    //   unsigned long nowTime = getTime(); // 记录开始时间
-    //   unsigned long useTime = nowTime - sys.startTime;
-    //   uint16_t minutes = useTime / 60;
-  
-    //   char y_char[20], command[80];
-    //   uint8_t return_data[8];
-    //   gc_execute_line("G90G53G0Z-5");
-    //   set_flip(0);
-    //   protocol_buffer_synchronize();
-    //   set_rfid(0);
-    //   memset(return_data, 0, 8);
-    //   // 移动刀位置
-    //   float2string(settings.tool_y[settings.tool - 1] - settings.rfid_offset, y_char, 3);
-    //   sprintf(command, "G90G53G0Y%s", y_char);
-    //   gc_execute_line(command);
-    //   protocol_buffer_synchronize();
-    //   rfid_write(settings.tool, minutes);
-    //   set_rfid(1);
-    // }
     set_flip(1);
     get_tool(tool_number);
     set_tool_length();
